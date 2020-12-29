a13e21e91b1f2491e4566a373d43db4d58b41c62
amcgavin <alec.mcgavin@kogan.com.au>
2020-12-29 13:07:41 +1100

    Don't hold on to certificates indefinitely (#4)

--- src/django_cloudtask/utils.py
+++ src/django_cloudtask/utils.py
@@ -1,19 +1,28 @@
+import functools
+import time
+
 import google.auth.transport.requests
 from google.auth import jwt
 from google.oauth2.id_token import _GOOGLE_OAUTH2_CERTS_URL
 from google.oauth2.id_token import _fetch_certs as fetch_certs
 
-certs = None
+
+@functools.lru_cache(1)
+def _get_certs(_ttl_hash):
+    request = google.auth.transport.requests.Request()
+    return fetch_certs(request, _GOOGLE_OAUTH2_CERTS_URL)
+
+
+def get_certs():
+    """
+    Refresh certificates (roughly) every 12 hours.
+    """
+    return _get_certs(time.time() // (3600 * 12))
 
 
 def verify_jwt(token: str) -> bool:
-    global certs
-    if certs is None:
-        request = google.auth.transport.requests.Request()
-        certs = fetch_certs(request, _GOOGLE_OAUTH2_CERTS_URL)
-
     try:
-        decoded = jwt.decode(token, certs=certs)
+        decoded = jwt.decode(token, certs=get_certs())
         return decoded["iss"] == "https://accounts.google.com"
     except ValueError:
         return False
